6ae06f64c9913330fc74658ac59b579e04729128
aivarmagi <aivar.magi@outlook.com>
2015-09-11 23:49:09 +0300

    master - add index controller and security configuration

--- src/main/java/ee/aivar/AmazonBooksApplication.java
+++ src/main/java/ee/aivar/AmazonBooksApplication.java
@@ -1,4 +1,4 @@
-package books;
+package ee.aivar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/ee/aivar/books/AmazonBooksApplicationTests.java
+++ src/test/java/ee/aivar/books/AmazonBooksApplicationTests.java
@@ -1,5 +1,6 @@
-package books;
+package ee.aivar.books;
 
+import ee.aivar.AmazonBooksApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
