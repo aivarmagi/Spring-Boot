550a041fae12647c03d6d995cf221a0f0f2ec545
aivarmagi <aivar.magi@outlook.com>
2015-09-11 22:01:14 +0300

    add empty Spring Boot Initializr

---
+++ src/main/java/books/AmazonBooksApplication.java
@@ -0,0 +1,12 @@
+package books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonBooksApplication.class, args);
+    }
+}
